284be8d0af367c620555fa908b382b29adb20edd
heramb mishra <herambmishra223@gmail.com>
2022-05-25 13:54:02 +0530

    Modified Media.js,Tabs.js,Query.js,en.json and modified the UI

--- src/js/plugins/tab.js
+++ src/js/plugins/tab.js
@@ -1,8 +1,12 @@
+/** ------- Tab management commands ------- */
+// Some Commands will be performed after a delay
+
 import { performActionWithDelay } from '../core';
+
+// to check the actions: go to src/js/langs/en.json
 
 const commands = [
   {
-    /** ------- Tab management commands ------- */
     action: 'TABS_NEW_TAB',
     callback: () => {
       performActionWithDelay(() => {
@@ -10,13 +14,6 @@ const commands = [
       });
     }
   },
-  {
-    action: 'INCOGNITO_WINDOW',
-    callback: () => {
-      chrome.windows.create({incognito: true});
-    }
-  }
-  ,
   {
     action: 'TABS_CLOSE_TAB',
     callback: () => {
@@ -221,7 +218,13 @@ const commands = [
       });
     }
   },
-
+  {
+    action: 'INCOGNITO_WINDOW',
+    callback: () => {
+      chrome.windows.create({incognito: true});
+    }
+  }
+  ,
   {
     action: 'TABS_MINIMIZE_WINDOW',
     callback: () => {
